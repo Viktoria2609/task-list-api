dc306689771ce0b959bb3c86ee09398e60cfe8ad
Viktoriia <lyudkevych.v@gmail.com>
2025-05-02 16:19:27 -0700

    Model instance "from_dict" created.

--- app/models/task.py
+++ app/models/task.py
@@ -6,4 +6,19 @@ class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
-    completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    is_complete: Mapped[datetime] = mapped_column(nullable=True)
+
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "title" : self.title,
+            "description" : self.description,
+            "is_complete" : self.is_complete
+        }
+    
+    @classmethod
+    def from_dict(cls, request_body):
+        return cls(
+            title = request_body["Name"],
+            description = request_body["description"],
+        )
