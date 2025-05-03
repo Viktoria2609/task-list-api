00b1f5537f71039bef5cce08191469df68af0e18
Viktoriia <lyudkevych.v@gmail.com>
2025-05-02 22:33:55 -0700

    Class Task changed to expected logic for passing wave1 tests.

--- app/models/task.py
+++ app/models/task.py
@@ -6,19 +6,19 @@ class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
-    is_complete: Mapped[datetime] = mapped_column(nullable=True)
+    completed_at: Mapped[datetime] = mapped_column(nullable=True)
 
     def to_dict(self):
         return {
             "id" : self.id,
             "title" : self.title,
             "description" : self.description,
-            "is_complete" : self.is_complete
+            "is_complete" : self.completed_at is not None
         }
     
     @classmethod
     def from_dict(cls, request_body):
         return cls(
-            title = request_body["Name"],
-            description = request_body["description"],
+            title = request_body["title"],
+            description = request_body["description"]
         )
