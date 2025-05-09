1050099fdf5c1355373148e0ce718735722941df
Viktoriia <lyudkevych.v@gmail.com>
2025-05-08 17:34:13 -0700

    Relationship created

--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,6 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import Optional
 from ..db import db
 from datetime import datetime
 
@@ -7,13 +9,16 @@ class Task(db.Model):
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
     def to_dict(self):
         return {
             "id" : self.id,
             "title" : self.title,
             "description" : self.description,
-            "is_complete" : self.completed_at is not None
+            "is_complete" : self.completed_at is not None,
+            "goal_id": self.goal_id
         }
     
     @classmethod
